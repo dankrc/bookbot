1a06d778de48536f1f2c985b8640534186020bab
dankrc <danielkim5998@gmail.com>
2024-11-09 22:16:33 -0500

    added char count

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ def main():
     print(text)
     counted_words = word_count(text)
     print(counted_words)
+    num_chars = char_count(text)
+    print(num_chars)
+
 def word_count(text):
     words = text.split()
     return len(words)
 
-
+def char_count(text):
+    lowered_text = text.lower()
+    chars = set(lowered_text)
+    num_chars = {}
+    for char in chars:
+        num_chars[char] = 0
+    for char in lowered_text:
+        num_chars[char] += 1
+    return num_chars
 
 
 def get_book_text(path):
